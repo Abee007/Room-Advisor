bcae5be98f21ec94013255e37c28cdcac44bae28
Abraham Mensah <abraham.mensah@yale.edu>
2022-03-08 15:40:26 -0500

    Update serverIp

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const serverIp = "https://room-advisor-v0-backend.herokuapp.com";
+export const serverIp = "http://server.abrahammensah.com";
 export const clientIp = "https://room-advisor-v0.web.app/";
 export const devServerIp = "http://localhost:4000";
