e376bf51a35182525dbb40f821edd3a646f1bd73
Abraham Mensah <abraham.mensah@yale.edu>
2022-03-08 16:27:48 -0500

    add https to server domain

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
-export const serverIp = "http://server.abrahammensah.com";
+export const serverIp = "https://server.abrahammensah.com";
 export const clientIp = "https://room-advisor-v0.web.app/";
 export const devServerIp = "http://localhost:4000";
