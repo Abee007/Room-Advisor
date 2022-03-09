8bf1a2f9783a714b10797b2bd6c5bcaa74ce1638
Abraham Mensah <abraham.mensah@yale.edu>
2022-03-08 20:22:38 -0500

    Update clientIp in frontend

--- frontend/src/constants.js
+++ frontend/src/constants.js
@@ -1,3 +1,3 @@
 export const serverIp = "https://server.abrahammensah.com";
-export const clientIp = "https://room-advisor-v0.web.app/";
+export const clientIp = "https://abrahammensah.com";
 export const devServerIp = "http://localhost:4000";
