ee38f0093669ba8b7c0d2e2d1e20c316a7318735
Abee007 <Abee007@users.noreply.github.com>
2022-04-18 03:19:21 +0000

    Prettified Code!

--- frontend/src/components/ViewReviews/Suites/BedroomModalBarChart.js
+++ frontend/src/components/ViewReviews/Suites/BedroomModalBarChart.js
@@ -14,10 +14,7 @@ import {
 export default function BarChartComponent({ data }) {
   return (
     <ResponsiveContainer>
-      <BarChart
-        data={data}
-        margin={{ top: 5, right: 20, left: 0, bottom: 5 }}
-      >
+      <BarChart data={data} margin={{ top: 5, right: 20, left: 0, bottom: 5 }}>
         <CartesianGrid strokeDasharray="3 3" />
         <XAxis dataKey="argument" interval={0} />
         <YAxis />
